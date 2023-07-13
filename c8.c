ccde57f88ae4f5b198d0945ea1142dbc0c7bf840
Ayushi <116718528+Ayushinitp@users.noreply.github.com>
2023-07-13 18:41:06 +0530

    Add files via upload

---
+++ c8.c
@@ -0,0 +1,140 @@
+//1
+// #include <stdio.h>
+// int main(){
+//     int i,n;
+//     printf("Enter any number : ");
+//     scanf("%d",&n);
+//     for(i=1;i<=n;i++)
+//         printf("NIT Patna\n");
+//         return 0;
+// }
+
+
+
+
+//2
+// #include <stdio.h>
+//  int main(){
+//     int i,n;
+//      printf("Enter any number : ");
+//     scanf("%d",&n);
+//     for(i=1;i<=n;i++)
+//     printf("%d\t",i);
+//     return 0;
+//  }
+
+
+
+
+
+//3
+// #include <stdio.h>
+//  int main(){
+//     int i,n;
+//      printf("Enter any number : ");
+//     scanf("%d",&n);
+//     for(i=n;i>0;i--)
+//     printf("%d\t",i);
+//     return 0;
+//  }
+
+
+
+
+//4
+// #include <stdio.h>
+//  int main(){
+//      int i,n;
+//       printf("Enter any number : ");
+//     scanf("%d",&n);
+//     for(i=1;i<2*n;i++){
+//         if(i%2)
+//         printf("%d\t",i);
+//     }
+//     return 0;
+//  }
+
+
+
+
+//5
+// #include <stdio.h>
+//  int main(){
+//      int i,n;
+//       printf("Enter any number : ");
+//     scanf("%d",&n);
+//     for(i=2*n;i>0;i--){
+//     if(i%2)
+//     printf("%d\t",i);
+//     }
+//     return 0;
+//  }
+
+
+
+
+//6
+// #include <stdio.h>
+//  int main(){
+//      int i,n;
+//       printf("Enter any number : ");
+//     scanf("%d",&n);
+//     for(i=2;i<=2*n;i++){
+//         if(i%2==0)
+//             printf("%d\t",i);
+//     }
+//     return 0;
+//  }
+
+
+
+
+
+//7
+// #include <stdio.h>
+//  int main(){
+//      int i,n;
+//       printf("Enter any number : ");
+//     scanf("%d",&n);
+//     for(i=2*n;i>0;i--){
+//         if(i%2==0)
+//         printf("%d\t",i);
+//     }
+//     return 0;
+//  }
+
+
+
+
+//8 , 9
+// #include <stdio.h>
+//  int main(){
+//     int i,n;
+//      printf("Enter any number : ");
+//     scanf("%d",&n);
+//     printf("Square of nos are : \n");
+//     for(i=1;i<=n;i++){
+//     printf("%d\t",i*i);
+//     }
+//     printf("\nCube of nos are : \n");
+//     for(i=1;i<=n;i++){
+//         printf("%d\t",i*i*i);
+//     }
+//     return 0;
+//  }
+
+
+
+
+//10
+// #include <stdio.h>
+//  int main(){
+//      int i,n;
+//       printf("Enter any number : ");
+//     scanf("%d",&n);
+//     printf("Table of %d is : \n",n);
+//     for(i=1;i<=10;i++){
+//         printf("%d*%d=%d\n",n,i,n*i);
+//     }
+//     return 0;
+//  }
